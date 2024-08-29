977a8caa33bdc1ef8e225c01ff89b9e617add0c7
Kshitij K <kshitijkk15@gmail.com>
2024-08-29 21:20:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+import Info from "./components/Info/Info";
+import Lounge from "./components/Lounge/Lounge";
+import Navbar from "./components/Navbar/Navbar";
+import Search from "./components/Search/Search";
+import Subscribers from "./components/Subscribers/Subscribers";
+import Support from "./components/Support/Support";
+import Travelers from "./components/Travelers/Travelers";
+import Login from "./components/Login";
+
+
+function App() {
+  return (
+    <main>
+      {/* <Navbar /> */}
+      {/* <Home /> */}
+      {/* <Search /> */}
+      {/* <Support /> */}
+      {/* <Info /> */}
+      {/* <Lounge /> */}
+      {/* <Travelers /> */}
+      {/* <Subscribers /> */}
+      <Login/>
+      {/* <Footer /> */}
+    </main>
+  );
+}
+
+export default App;
